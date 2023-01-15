6329ff267f504d0e4f9fd3af00608efab2c9506f
MartinezFabian <FabianMartinezDev@gmail.com>
2023-01-15 20:55:20 -0300

    reduce image size and convert to webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 // compilar Sass
 
+//dependencias CSS Y Sass
 const { src, dest, watch, series, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+
+//dependencias Imagenes
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
 
 function css(done) {
   // identificar archivo scss, luego compilarlo y por ultimo guardar el .css
@@ -17,9 +22,18 @@ function css(done) {
 
 //Tarea para tratamiento de imagenes
 function images(done) {
-  src("src/img/**/*").pipe(dest("build/img"));
+  src("src/img/**/*")
+    .pipe(imagemin({ optimizationLevel: 3 }))
+    .pipe(dest("build/img"));
 
   done(); // indica que aqui acaba la tarea
+}
+
+//Tarea para convertir imagenes en webp
+function convertToWebp(done) {
+  src("src/img/**/*.{png,jpg}").pipe(webp()).pipe(dest("build/img"));
+
+  done();
 }
 
 // Watch
@@ -34,6 +48,7 @@ function dev() {
 exports.css = css; //usamos exports para que sea ejecutable por Gulp
 exports.dev = dev;
 exports.images = images;
+exports.convertToWebp = convertToWebp;
 
-exports.default = series(images, css, dev);
+exports.default = series(images, convertToWebp, css, dev);
 //exports.default = parallel(css, dev);
